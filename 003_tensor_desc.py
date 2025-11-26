9161a3f4a2c1c4b399eb29fc37fce3a94cbe43d8
KindRoach <kindroach@hotmail.com>
2025-11-26 12:37:21 +0000

    Update tensor_descriptor eexample.

---
+++ 003_tensor_desc.py
@@ -0,0 +1,117 @@
+import torch
+import triton
+import triton.language as tl
+
+from utils import acc_check
+
+
+@triton.jit
+def copy_1D_kernel(
+    input_ptr: tl.pointer_type,
+    output_ptr: tl.pointer_type,
+    N: int,
+    BLOCK: tl.constexpr,
+):
+    pid = tl.program_id(0)
+    input_desc = tl.make_tensor_descriptor(
+        base=input_ptr,
+        shape=[N],
+        strides=[1],
+        block_shape=[BLOCK],
+    )
+
+    output_desc = tl.make_tensor_descriptor(
+        base=output_ptr,
+        shape=[N],
+        strides=[1],
+        block_shape=[BLOCK],
+    )
+
+    x = input_desc.load([pid * BLOCK])
+    output_desc.store([pid * BLOCK], x)
+
+
+def copy_1D():
+    print(f"{'='*20} 1D copy {'='*20}")
+    N = 1024 + 1  # Example size not divisible by BLOCK
+    BLOCK = 128
+    num_blocks = (N + BLOCK - 1) // BLOCK
+
+    device = "cuda"
+    dtype = torch.float32
+    input_tensor = torch.randn(N, dtype=dtype, device=device)
+    output_tensor = torch.empty_like(input_tensor)
+
+    copy_1D_kernel[(num_blocks,)](
+        input_tensor,
+        output_tensor,
+        N,
+        BLOCK,
+    )
+
+    acc_check(input_tensor, output_tensor)
+
+
+@triton.jit
+def copy_2D_kernel(
+    input_ptr: tl.pointer_type,
+    output_ptr: tl.pointer_type,
+    N: int,
+    M: int,
+    BLOCK_M: tl.constexpr,
+    BLOCK_N: tl.constexpr,
+):
+    pid_m = tl.program_id(0)
+    pid_n = tl.program_id(1)
+
+    input_desc = tl.make_tensor_descriptor(
+        base=input_ptr,
+        shape=[N, M],
+        strides=[M, 1],
+        block_shape=[BLOCK_M, BLOCK_N],
+    )
+
+    output_desc = tl.make_tensor_descriptor(
+        base=output_ptr,
+        shape=[N, M],
+        strides=[M, 1],
+        block_shape=[BLOCK_M, BLOCK_N],
+    )
+
+    row_offset = pid_m * BLOCK_M
+    col_offset = pid_n * BLOCK_N
+
+    x = input_desc.load([row_offset, col_offset])
+    output_desc.store([row_offset, col_offset], x)
+
+
+def copy_2D():
+    print(f"{'='*20} 2D copy {'='*20}")
+    N = 65  # Example rows not divisible by BLOCK_M
+    M = 130  # Example cols not divisible by BLOCK_N
+    BLOCK_M = 16
+    BLOCK_N = 32
+
+    num_blocks_m = (N + BLOCK_M - 1) // BLOCK_M
+    num_blocks_n = (M + BLOCK_N - 1) // BLOCK_N
+
+    device = "cuda"
+    dtype = torch.float32
+    input_tensor = torch.randn(N, M, dtype=dtype, device=device)
+    output_tensor = torch.empty_like(input_tensor)
+
+    copy_2D_kernel[(num_blocks_m, num_blocks_n)](
+        input_tensor,
+        output_tensor,
+        N,
+        M,
+        BLOCK_M,
+        BLOCK_N,
+    )
+
+    acc_check(input_tensor, output_tensor)
+
+
+if __name__ == "__main__":
+    copy_1D()
+    copy_2D()
