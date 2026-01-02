ff95b191fef394aeb3d88d4b6b27e36801e22819
KindRoach <kindroach@hotmail.com>
2026-01-02 06:43:22 +0000

    Change single token to token_centric impl.

--- src/05_moe/04_moe_token_centric.py
+++ src/05_moe/04_moe_token_centric.py
@@ -5,11 +5,11 @@ from .utils import generate_moe_inputs, ref_topk_routing
 from ..utils import acc_check, bench_by_secs, get_device
 
 
-def ref_moe_single_token(
-    hiddens: torch.Tensor,  # [1, H]
-    out: torch.Tensor,  # [1, H]
-    expert_ids: torch.Tensor,  # [1, K]
-    expert_weights: torch.Tensor,  # [1, K]
+def ref_moe_token_centric(
+    hiddens: torch.Tensor,  # [T, H]
+    out: torch.Tensor,  # [T, H]
+    expert_ids: torch.Tensor,  # [T, K]
+    expert_weights: torch.Tensor,  # [T, K]
     w_gate_up: torch.Tensor,  # [E, H, 2*I]
     w_down: torch.Tensor,  # [2*I, H]
 ):
@@ -18,26 +18,25 @@ def ref_moe_single_token(
     E, _, I2 = w_gate_up.shape
     I = I2 // 2
 
-    assert T == 1, "This reference implementation only supports single token (T=1)."
+    for t in range(T):
+        # gate up
+        w_gate_up_selected = w_gate_up[expert_ids[t]]  # [K, H, 2*I]
+        gate_up = hiddens[t].unsqueeze(0) @ w_gate_up_selected  # [K, 2*I]
 
-    # gate up
-    w_gate_up_selected = w_gate_up[expert_ids[0]]  # [K, H, 2*I]
-    gate_up = hiddens @ w_gate_up_selected  # [K, 1, 2*I]
+        # silu activation
+        gate_up = F.silu(gate_up[:, :, :I]) * gate_up[:, :, I:]  # [K, 1, I]
 
-    # silu activation
-    gate_up = F.silu(gate_up[:, :, :I]) * gate_up[:, :, I:]  # [K, 1, I]
+        # down
+        w_down_selected = w_down[expert_ids[t]]  # [K, I, H]
+        down = gate_up @ w_down_selected  # [K, 1, H]
 
-    # down
-    w_down_selected = w_down[expert_ids[0]]  # [2*I, H]
-    down = gate_up @ w_down_selected  # [K, 1, H]
-
-    # gather
-    out[:] = expert_weights @ down.squeeze(1)  # [1, H]
+        # gather
+        out[t, :] = expert_weights[t] @ down.squeeze(1)  # [H]
 
 
 @torch.inference_mode()
 def main():
-    T = 1
+    T = 4
     H = 2048
     I = 768
     E = 128
@@ -60,10 +59,10 @@ def main():
 
     topk_expert_ids, topk_expert_weights = ref_topk_routing(logits, top_k=top_k)
 
-    out_tensor = torch.empty(1, H, device=hiddens.device, dtype=hiddens.dtype)
+    out_tensor = torch.empty(T, H, device=hiddens.device, dtype=hiddens.dtype)
     ref_out = torch.empty_like(out_tensor)
 
-    ref_moe_single_token(
+    ref_moe_token_centric(
         hiddens,
         ref_out,
         topk_expert_ids,
@@ -74,7 +73,7 @@ def main():
 
     # perform benchmark
     funcs_to_bench = {
-        ref_moe_single_token.__name__: lambda: ref_moe_single_token(
+        ref_moe_token_centric.__name__: lambda: ref_moe_token_centric(
             hiddens,
             out_tensor,
             topk_expert_ids,
